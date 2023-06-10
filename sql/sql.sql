bc171cdc2424ec5b7190ab61b54a51dbc2a5fd91
luckylau <laujunbupt0203@163.com>
2023-06-10 13:38:53 +0800

    sql: user

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,19 +1,19 @@
 CREATE DATABASE IF NOT EXISTS learn
   DEFAULT CHARACTER SET = utf8mb4;
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `users`;
 
 
-Users	CREATE TABLE `Users` (
-  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增Id',
-  `Username` varchar(64) NOT NULL DEFAULT 'default' COMMENT '用户名',
-  `Password` varchar(64) NOT NULL DEFAULT 'default' COMMENT '密码',
-  `Email` varchar(64) NOT NULL DEFAULT 'default' COMMENT '邮箱地址',
-  `Enabled` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否有效',
-  `Role` varchar(32) NOT NULL DEFAULT '',
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `username` varchar(64) NOT NULL DEFAULT 'default' COMMENT '用户名',
+  `password` varchar(64) NOT NULL DEFAULT 'default' COMMENT '密码',
+  `email` varchar(64) NOT NULL DEFAULT 'default' COMMENT '邮箱地址',
+  `enabled` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否有效',
+  `role` varchar(32) NOT NULL DEFAULT '',
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户表'
 
-INSERT INTO `Users` (`Username`, `Password`, `Email`, `Enabled`, `Role`)
+INSERT INTO `users` (`username`, `password`, `email`, `enabled`, `role`)
 VALUES
   ('learn', '$2a$10$TTXd9kv6tlwEk5x0ZLgnGu7/c18MJv3nHP36IKo7y.FuwNctizbLW
 ', 'laujunbupt0916@163.com', 1,'ROLE_ADMIN');
